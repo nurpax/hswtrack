3b0b23d80a5bf131cc342c34564363168fc06baa
Janne Hellsten <jjhellst@gmail.com>
2014-01-12 16:01:28 +0200

    Upgrade to Handlebars 1.3

--- static/main.js
+++ static/main.js
@@ -4,10 +4,19 @@ requirejs.config({
         d3: 'lib/d3/d3.v3.min',
         jquery: 'lib/jquery-2.0.3/jquery.min',
         bootstrap: 'lib/bootstrap-3.0.0/js/bootstrap.min',
-        handlebars: 'lib/handlebars-1.0.0/handlebars',
+        handlebars: 'lib/handlebars-1.3.0/handlebars-v1.3.0',
         router: 'lib/router'
     },
     shim: {
+        // TODO I don't understand why this is needed here.
+        // Handlebars 1.3 is supposed to work fine with RequireJS but
+        // if I don't add this exports here, Handlebars is not defined
+        // in app/app.js App constructor, even though it's defined as
+        // a dependency.
+        //
+        // I think I should install Handlebars via npm, perhaps that
+        // installer adds the right .amd files for this to work the
+        // right way with requirejs.
         handlebars: {
             exports: 'Handlebars'
         },
