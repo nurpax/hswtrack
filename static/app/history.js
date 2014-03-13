0698e85c1348e47480b70498438e8cd8e9982c09
Janne Hellsten <jjhellst@gmail.com>
2014-03-13 21:59:10 +0200

    Show total reps for body-weight exercises in the history view

--- static/app/history.js
+++ static/app/history.js
@@ -1,4 +1,4 @@
-define(['jquery', 'handlebars'], function($, Handlebars) {
+define(['jquery', 'handlebars', 'underscore'], function($, Handlebars, _) {
     "use strict";
 
     // View workout history
@@ -19,6 +19,17 @@ define(['jquery', 'handlebars'], function($, Handlebars) {
     History.prototype.renderPastWorkouts = function (ws) {
         var self = this;
         var workouts = { workouts: ws };
+
+        // Compute some nice-to-know stats, like total reps count for each exercise
+        _.each(workouts.workouts,
+               function (w) {
+                   _.each(w.exercises, function (e) {
+                       if (e.type == "BW") {
+                           e.totalReps = _.reduce(e.sets, function (a, s) { return a+s.reps; }, 0);
+                       }
+                   });
+               });
+
         $("#app-container").html(self.mainTemplate(workouts));
     };
 
