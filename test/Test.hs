9d04fd3000bc717adb5a80c20e4bc0bedaecb218
Janne Hellsten <jjhellst@gmail.com>
2014-05-02 17:03:35 +0300

    Add createUserTests and loginUserTests combinators
    These can be used to create TestGroups that are run with cookies
    received from the login or create user procedure.

--- test/Test.hs
+++ test/Test.hs
@@ -7,28 +7,43 @@ import qualified Data.Text as T
 import           Network.Wreq
 import           Test.Framework
 import           Test.Framework.Providers.HUnit
-import           Test.HUnit
+import           Test.HUnit hiding (TestList, Test)
 
-testCreateUser :: Assertion
-testCreateUser = do
+-- | Create a user and run a list of subtests with cookies acquired
+-- from the login process.
+createUserTests :: [(String, Options -> Assertion)] -> IO Test
+createUserTests tests = do
   let login  = "test" :: T.Text
       passwd = "testpass" :: T.Text
   r <- post "http://localhost:8000/rest/new_user" ["login" := login, "password" := passwd]
-  1 @=? (1 :: Int)
+  let opts = defaults & cookies .~ (r ^. responseCookieJar)
+  print $ opts ^. cookies
+  return
+    . testGroup "Tests with created user"
+    . map (\(name, test) -> testCase name (test opts)) $ tests
 
-test1 :: Assertion
-test1 = do
+-- | Login a user and run a list of subtests with cookies acquired
+-- from the login process.
+loginUserTests :: [(String, Options -> Assertion)] -> IO Test
+loginUserTests tests = do
   let login  = "test" :: T.Text
       passwd = "testpass" :: T.Text
   r <- post "http://localhost:8000/rest/login" ["login" := login, "password" := passwd]
   let opts = defaults & cookies .~ (r ^. responseCookieJar)
+  print $ opts ^. cookies
+  return
+    . testGroup "Tests with logged in user"
+    . map (\(name, test) -> testCase name (test opts)) $ tests
+
+testLoggedInOk :: Options -> Assertion
+testLoggedInOk opts = do
   r <- getWith opts "http://localhost:8000/rest/app"
   print (r ^. responseBody)
   1 @=? (1 :: Int)
 
 main :: IO ()
 main = defaultMainWithOpts
-       [ testCase "createUser" testCreateUser
-       , testCase "login"      test1
+       [ buildTest $ createUserTests [("logged in?", testLoggedInOk)]
+       , buildTest $ loginUserTests  [("logged in?", testLoggedInOk)]
        ]
        mempty
